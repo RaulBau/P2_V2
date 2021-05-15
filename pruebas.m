d48ad592a72212faa38f1b4bb435efe26b7e4493
raulbau <mrrbau@gmail.com>
2021-05-15 04:10:55 -0500

    Se agregaron los filtros del segundo objeto

--- pruebas.m
+++ pruebas.m
@@ -1,3 +1,11 @@
+##
+# FACULTAD DE INGENIERÍA UASLP
+# Examen Parcial 2 v2
+# Nombre: Bautista Robles Raúl
+# Clave: 229563
+# Fecha: 15/05/2021
+##
+
 ##Se limpia la consola
 clc;
 clear;
@@ -11,7 +19,11 @@ info=aviinfo("10292009083301.avi");
 ##Se obtiene un frame en especifico
 ##vv=aviread("10292009083301.avi",1250);##p1
 ##vv=aviread("10292009083301.avi",2100);##p2
-vv=aviread("10292009083301.avi",4200);##p3
+##vv=aviread("10292009083301.avi",4200);##p3
+
+##vv=aviread("10292009083301.avi",1250);##d1
+##vv=aviread("10292009083301.avi",2100);##d2
+##vv=aviread("10292009083301.avi",3500);##d3
 
 ##Creación de Filtros
 
@@ -20,7 +32,11 @@ vv=aviread("10292009083301.avi",4200);##p3
 ##imFiltro=rgb2gray(imcrop(vv,[110,155,17,11]));##p2
 ##imFiltro=rgb2gray(imcrop(vv,[155,99,13,5]));##p3
 
-##  imO=rgb2gray(vv);
+##imFiltro=rgb2gray(imcrop(vv,[91,176,19,11]));##d1
+##imFiltro=rgb2gray(imcrop(vv,[169,145,17,9]));##d2
+##imFiltro=rgb2gray(imcrop(vv,[140,92,15,7]));##d3
+
+##imO=rgb2gray(vv);
 ##imshow(imO);
 ##return
 
@@ -28,20 +44,37 @@ vv=aviread("10292009083301.avi",4200);##p3
 ##imH=padarray(imFiltro,[64,60]);##p1
 ##imH=padarray(imFiltro,[64,61]);##p2
 ##imH=padarray(imFiltro,[67,63]);##p3
-##imshow(imFiltro);
+
+##imH=padarray(imFiltro,[64,60]);##d1
+##imH=padarray(imFiltro,[65,61]);##d2
+##imH=padarray(imFiltro,[66,62]);##d3
+
 ##imwrite(imH,"Filtros/p1.bmp");##p1
 ##imwrite(imH,"Filtros/p2.bmp");##p2
 ##imwrite(imH,"Filtros/p3.bmp");##p3
+
+##imwrite(imH,"Filtros/d1.bmp");##d1
+##imwrite(imH,"Filtros/d2.bmp");##d2
+##imwrite(imH,"Filtros/d3.bmp");##d3
+
+##imshow(imFiltro);
 ##return
 
 ##Fin Creación de Filtros
 
 
 ##Filtros p
-ruta="Filtros/p";
+rutaP="Filtros/p";
 LK=0.4;
 NImages=3;
-H_Filtro_P=filtroC_LK(LK,ruta,NImages,0.6);
+H_Filtro_P=filtroC_LK(LK,rutaP,NImages,0.6);
+##FinFiltros
+
+##Filtros d
+rutaD="Filtros/d";
+LK=0.4;
+NImages=3;
+H_Filtro_D=filtroC_LK(LK,rutaD,NImages,0.6);
 ##FinFiltros
 
 ##Creación de la mascara
@@ -58,7 +91,8 @@ cc=1;
 ##imH1=fft2(imH);
 ####Obtenemos la informaciï¿½n del video
 ##info=aviinfo("10292009083301.avi");
-arr_P=zeros(info.NumFrames/3,6);
+arr_P=zeros(info.NumFrames/3,7);
+arr_D=zeros(info.NumFrames/3,7);
 
 iCont=1;
 for iRec=1:3:info.NumFrames-130
@@ -72,7 +106,7 @@ for iRec=1:3:info.NumFrames-130
   ##imO=rgb2gray(imcrop(frm,[105,45,195,195]));
   imO=frm;
   
-  ##Aplicamos el filtro
+  ##Aplicamos el filtro P
   O_Filtro=kLawSpaceV(LK,frmRes);
   resp=O_Filtro.*(conj(H_Filtro_P)./abs(H_Filtro_P));
   corl=real(ifftshift(ifft2(resp)));
@@ -81,29 +115,36 @@ for iRec=1:3:info.NumFrames-130
   corl2=imfilter(corl,mascara);
   maximo2=max(corl2(:));
   [yy2,xx2]=find(maximo2==corl2);
-  arr_P(iCont,1)=xx1;
-  arr_P(iCont,2)=yy1;
-  arr_P(iCont,3)=maximo1;
-  arr_P(iCont,4)=xx2;
-  arr_P(iCont,5)=yy2;
-  arr_P(iCont,6)=maximo2;
+  arr_P(iCont,1)=iRec;
+  arr_P(iCont,2)=xx1;
+  arr_P(iCont,3)=yy1;
+  arr_P(iCont,4)=maximo1;
+  arr_P(iCont,5)=xx2;
+  arr_P(iCont,6)=yy2;
+  arr_P(iCont,7)=maximo2;
+  
+  ##Aplocamos el filtro D
+  O_Filtro=kLawSpaceV(LK,frmRes);
+  resp=O_Filtro.*(conj(H_Filtro_D)./abs(H_Filtro_D));
+  corl=real(ifftshift(ifft2(resp)));
+  maximo1=max(corl(:));
+  [yy1,xx1]=find(maximo1==corl);
+  corl2=imfilter(corl,mascara);
+  maximo2=max(corl2(:));
+  [yy2,xx2]=find(maximo2==corl2);
+  arr_D(iCont,1)=iRec;
+  arr_D(iCont,2)=xx1;
+  arr_D(iCont,3)=yy1;
+  arr_D(iCont,4)=maximo1;
+  arr_D(iCont,5)=xx2;
+  arr_D(iCont,6)=yy2;
+  arr_D(iCont,7)=maximo2;
   iCont=iCont+1;
 
-##  cor1=real(ifftshift(ifft2(imO.*conj(imH1./abs(imH1)))));
-##  vmax=reshape(cor1,[],1);
-##  vmax10=sort(vmax,'descend');
-##  [yy,xx]=find(vmax10(1)==cor1);
-##  arr(iCont,1)=xx;
-##  arr(iCont,2)=yy;
-##  arr(iCont,3)=vmax10(1);
-##  [yy,xx]=find(vmax10(2)==cor1);
-##  arr(iCont,4)=xx;
-##  arr(iCont,5)=yy;
-##  arr(iCont,6)=vmax10(2);
-##  iCont=iCont+1;
 endfor
 
 csvwrite("res_P.csv",arr_P);
+csvwrite("res_D.csv",arr_D);
 
 
 
