a81a680f285e23f7e2057a9fc003dbd76d446b69
raulbau <mrrbau@gmail.com>
2021-05-15 03:22:32 -0500

    Se agregaron los filtros de la ley k

--- pruebas.m
+++ pruebas.m
@@ -13,62 +13,97 @@ info=aviinfo("10292009083301.avi");
 ##vv=aviread("10292009083301.avi",2100);##p2
 vv=aviread("10292009083301.avi",4200);##p3
 
-##Filtros
+##Creación de Filtros
 
 ##Se recorta la imagen del filtro
 ##imFiltro=rgb2gray(imcrop(vv,[157,192,19,11]));##p1
 ##imFiltro=rgb2gray(imcrop(vv,[110,155,17,11]));##p2
-imFiltro=rgb2gray(imcrop(vv,[155,99,13,5]));##p3
+##imFiltro=rgb2gray(imcrop(vv,[155,99,13,5]));##p3
 
-##Fin Filtros
-
-##Se recorta la imagen del fondo
-imO=rgb2gray(imcrop(vv,[105,45,195,195]));
-imO=rgb2gray(vv);
-
+##  imO=rgb2gray(vv);
 ##imshow(imO);
 ##return
 
 ##Se completa la imagen para hacerla cuadrada
 ##imH=padarray(imFiltro,[64,60]);##p1
 ##imH=padarray(imFiltro,[64,61]);##p2
-imH=padarray(imFiltro,[67,63]);##p3
-imshow(imFiltro);
+##imH=padarray(imFiltro,[67,63]);##p3
+##imshow(imFiltro);
 ##imwrite(imH,"Filtros/p1.bmp");##p1
 ##imwrite(imH,"Filtros/p2.bmp");##p2
-imwrite(imH,"Filtros/p3.bmp");##p3
-return
+##imwrite(imH,"Filtros/p3.bmp");##p3
+##return
 
-##Leemos el filtro
-imH=imread("filtro.bmp");
-##PAsamos la imagen al dominio de furier
-imH1=fft2(imH);
-##Obtenemos la informaciï¿½n del video
-info=aviinfo("10292009083301.avi");
-arr=zeros(info.NumFrames/3,6);
+##Fin Creación de Filtros
+
+
+##Filtros p
+ruta="Filtros/p";
+LK=0.4;
+NImages=3;
+H_Filtro_P=filtroC_LK(LK,ruta,NImages,0.6);
+##FinFiltros
+
+##Creación de la mascara
+mascara=ones(3);
+mascara=mascara*-1;
+mascara(2,2)=10;
+cc=1;
+##Fin Creación mascara
+
+
+####Leemos el filtro
+##imH=imread("filtro.bmp");
+####PAsamos la imagen al dominio de furier
+##imH1=fft2(imH);
+####Obtenemos la informaciï¿½n del video
+##info=aviinfo("10292009083301.avi");
+arr_P=zeros(info.NumFrames/3,6);
 
 iCont=1;
 for iRec=1:3:info.NumFrames-130
-  im1=rgb2gray(aviread("10292009083301.avi",iRec));
-  imO=imcrop(im1,[105,45,195,195]);
-  imF=imO;
-  imO=fft2(imO);
+  ##Obtenemos el frame
+  frm=rgb2gray(aviread("10292009083301.avi",iRec));
+  ##Recortamos la imagen
+  frm=imcrop(frm,[95,85, 139, 139]);
+  ##Creamos un respaldo del frame
+  frmRes=frm;
+  ##Se recorta la imagen del fondo
+  ##imO=rgb2gray(imcrop(frm,[105,45,195,195]));
+  imO=frm;
   
-  cor1=real(ifftshift(ifft2(imO.*conj(imH1./abs(imH1)))));
-  vmax=reshape(cor1,[],1);
-  vmax10=sort(vmax,'descend');
-  [yy,xx]=find(vmax10(1)==cor1);
-  arr(iCont,1)=xx;
-  arr(iCont,2)=yy;
-  arr(iCont,3)=vmax10(1);
-  [yy,xx]=find(vmax10(2)==cor1);
-  arr(iCont,4)=xx;
-  arr(iCont,5)=yy;
-  arr(iCont,6)=vmax10(2);
+  ##Aplicamos el filtro
+  O_Filtro=kLawSpaceV(LK,frmRes);
+  resp=O_Filtro.*(conj(H_Filtro_P)./abs(H_Filtro_P));
+  corl=real(ifftshift(ifft2(resp)));
+  maximo1=max(corl(:));
+  [yy1,xx1]=find(maximo1==corl);
+  corl2=imfilter(corl,mascara);
+  maximo2=max(corl2(:));
+  [yy2,xx2]=find(maximo2==corl2);
+  arr_P(iCont,1)=xx1;
+  arr_P(iCont,2)=yy1;
+  arr_P(iCont,3)=maximo1;
+  arr_P(iCont,4)=xx2;
+  arr_P(iCont,5)=yy2;
+  arr_P(iCont,6)=maximo2;
   iCont=iCont+1;
+
+##  cor1=real(ifftshift(ifft2(imO.*conj(imH1./abs(imH1)))));
+##  vmax=reshape(cor1,[],1);
+##  vmax10=sort(vmax,'descend');
+##  [yy,xx]=find(vmax10(1)==cor1);
+##  arr(iCont,1)=xx;
+##  arr(iCont,2)=yy;
+##  arr(iCont,3)=vmax10(1);
+##  [yy,xx]=find(vmax10(2)==cor1);
+##  arr(iCont,4)=xx;
+##  arr(iCont,5)=yy;
+##  arr(iCont,6)=vmax10(2);
+##  iCont=iCont+1;
 endfor
 
-csvwrite("pruebas.csv",arr);
+csvwrite("res_P.csv",arr_P);
 
 
 
